038672f328bd42e30031bbcc3dc79fbbcf182dd6
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-03-30 11:43:47 -0400

    debuggin code

--- dst/settings.py
+++ dst/settings.py
@@ -128,5 +128,3 @@ STATICFILES_FINDERS = ("compressor.finders.CompressorFinder")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-COMPRESS_OFFLINE = True
-STATICFILES_STROAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
