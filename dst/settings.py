9ec7805b0c97e396f7542de9bdefd19f4c876e78
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-03-30 11:48:00 -0400

    settings.py debug

--- dst/settings.py
+++ dst/settings.py
@@ -122,7 +122,6 @@ MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
-STATICFILES_FINDERS = ("compressor.finders.CompressorFinder")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
