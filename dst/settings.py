438286af3b79f1e5b87871f0ddf6abbd9d16000b
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-03-30 11:32:37 -0400

    static settings

--- dst/settings.py
+++ dst/settings.py
@@ -116,14 +116,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
-# Extra places for collectstatic to find staticfiles.
-STATICFILE_DIRS = [
-    BASE_DIR  / 'static/',
-    'var/www/static',
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
